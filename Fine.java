21d7ea32595d8b2ba0ecf7a139aff97a26b153ce
lshreyasharmal <shreya15096@iiitd.ac.in>
2017-04-15 02:33:07 +0530

    Update Fine.java

--- Fine.java
+++ Fine.java
@@ -2,15 +2,12 @@ import java.sql.Date;
 
 public class Fine {
 	private int Borrow_ID, Fine_Rate, Fine_Amount;
-	private Date Date_Issued, Return_Date;
 	
-	public Fine(int Borrow_ID, int Fine_Rate, int Fine_Amount , Date Date_Issued, Date Return_Date){
+	public Fine(int Borrow_ID, int Fine_Rate, int Fine_Amount){
 		super();
 		this.Borrow_ID=Borrow_ID;
 		this.Fine_Rate=Fine_Rate;
 		this.Fine_Amount=Fine_Amount;
-		this.Date_Issued=Date_Issued;
-		this.Return_Date=Return_Date;
 	}
 	int getBorrow_ID()
 	{
@@ -22,14 +19,6 @@ public class Fine {
 	int getFine_Amount()
 	{
 		return this.Fine_Amount;
-	}
-	Date getDate_Issued()
-	{
-		return this.Date_Issued;
-	}
-	Date getReturn_Date()
-	{
-		return this.Return_Date;
 	}
 	
 	void setBorrow_ID(int x)
@@ -43,19 +32,11 @@ public class Fine {
 	{
 		this.Fine_Amount=x;
 	}
-	void setDate_Issued(Date x)
-	{
-		Date_Issued=x;
-	}
-	void setReturn_Date( Date x)
-	{
-		this.Return_Date=x;
-	}
 	
 	@Override
 	public String toString() {
-		return String.format("Fine [Borrow_ID=%d, Fine_Rate=%d, Fine_Amount=%d, Date_Issued=%s, Return_Date=%s]",
-				Borrow_ID, Fine_Rate, Fine_Amount, Date_Issued, Return_Date);
+		return String.format("Fine [Borrow_ID=%d, Fine_Rate=%d, Fine_Amount=%d]",
+				Borrow_ID, Fine_Rate, Fine_Amount);
 	}
 	
 	
